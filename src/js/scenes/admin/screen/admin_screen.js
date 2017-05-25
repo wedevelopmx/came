d0ad8be0cd34c8e74779b7f1c007d64475704df1
Cristian Colorado <ccoloradoc@gmail.com>
2017-05-24 23:38:57 -0500

    Tab: Implementing tab component

--- src/js/scenes/admin/screen/admin_screen.js
+++ src/js/scenes/admin/screen/admin_screen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import VisitorsCard from 'search/containers/container_visitors_card';
+import { Tabs, Tab }from 'commons/tab';
 
 export default function() {
   return (
@@ -9,7 +10,19 @@ export default function() {
         <VisitorsCard />
       </div>
       <div className="col-half">
-
+        <div className="box">
+          <Tabs selected="1">
+            <Tab title="Alerta" icon="supervisor_account">
+              <h3>One</h3>
+            </Tab>
+            <Tab title="Acompanamiento" icon="launch">
+              <h3>Two</h3>
+            </Tab>
+            <Tab title="Salidas" icon="warning">
+              <h3>Three</h3>
+            </Tab>
+          </Tabs>
+        </div>
       </div>
     </div>
   );
--- src/js/scenes/register/components/component_visitor_card.js
+++ src/js/scenes/register/components/component_visitor_card.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import VisitorCardProfile from '../containers/container_visitor_card_profile';
-import DoorInsight from '../containers/container_door_insight';
+import { Tabs, Tab }from 'commons/tab';
+import CommentsInsight from '../containers/container_comments_insight';
+import FollowUpInsigth from '../containers/container_followup_insight';
+import ExitInsigth from '../containers/container_exit_insight';
+
 
 class VisitorCard extends Component {
   render() {
     return (
       <div className="box widget">
-        <VisitorCardProfile/>
-        <DoorInsight/>
+        <VisitorCardProfile>
+          <Tabs selected="1">
+            <Tab title="Alerta" icon="supervisor_account">
+              <FollowUpInsigth></FollowUpInsigth>
+            </Tab>
+            <Tab title="Acompanamiento" icon="launch">
+              <ExitInsigth></ExitInsigth>
+            </Tab>
+            <Tab title="Salidas" icon="warning">
+              <CommentsInsight></CommentsInsight>
+            </Tab>
+          </Tabs>
+        </VisitorCardProfile>
       </div>
     );
   }
