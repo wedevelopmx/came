40c7e66a3cb9dcc37a7c05342ac973bcb773c0ef
Cristian Colorado <ccoloradoc@gmail.com>
2017-05-26 12:45:30 -0500

    Crud Compoent: Component to hold crud components

---
+++ src/js/components/commons/flipper/components/component_crud.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+// <Crud list={ComponentList} create={ComponentForm} update={ComponentUpdate}/>
+
+class CrudComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeModal: false, activeItem: undefined };
+    this.hideModal = this.hideModal.bind(this);
+    this.showModal = this.showModal.bind(this);
+  }
+
+  hideModal() {
+    this.setState({
+      activeModal: false,
+      activeItem: undefined
+    });
+  }
+
+  showModal(item) {
+    this.setState({
+      activeModal: true,
+      activeItem: item
+    });
+  }
+
+  renderModal() {
+    const { create : ComponentForm, update : ComponentUpdate } = this.props;
+
+    // Modal is active
+    if(this.state.activeModal) {
+      // There is an item to be displayed
+      if(this.state.activeItem) {
+        return (
+          <div className="overlay-modal">
+            <div>
+              <ComponentUpdate onComplete={ this.hideModal } />
+            </div>
+          </div>
+        );
+      } else {
+        // There is not initial item
+        return (
+          <div className="overlay-modal">
+            <div>
+              <ComponentForm onComplete={ this.hideModal } />
+            </div>
+          </div>
+        );
+      }
+    }
+
+    return ( <div></div> );
+  }
+
+  render() {
+    const { list : ComponentList } = this.props;
+
+    return (
+      <div>
+          <ComponentList onCreate={ this.showModal } onUpdate={ this.showModal }/>
+          { this.renderModal() }
+      </div>
+    );
+  }
+}
+
+export default CrudComponent;
--- src/js/components/commons/flipper/index.js
+++ src/js/components/commons/flipper/index.js
@@ -1,2 +1,3 @@
 export { default as Flipper } from './components/component_flipper';
 export { default as FlipperModal } from './components/component_flipper_modal';
+export { default as Crud } from './components/component_crud';
