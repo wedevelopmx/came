bc8e6ffa4caadfa4128d235fa933d3a197e1d7ac
Cristian Colorado <ccoloradoc@gmail.com>
2017-05-29 11:57:43 -0500

    Removing unused files

--- src/js/scenes/register/index.js
+++ src/js/scenes/register/index.js
@@ -2,13 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { HashRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
-
-import VisitorNew from './screens/visitor_new';
-import VisitorShow from './screens/visitor';
 import VisitorList from './screens/visitor_list';
-
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
--- src/js/scenes/register/screens/visitor.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default function() {
-  return (
-      <h1>Visitor</h1>
-  );
-}
