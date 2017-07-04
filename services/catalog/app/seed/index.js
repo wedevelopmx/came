2fc1cc5c0ec6de018a232d79963aeddf537a3d45
Cristian Colorado <ccoloradoc@gmail.com>
2017-07-04 18:58:12 -0500

    Adding catalog options

--- services/catalog/app/seed/index.js
+++ services/catalog/app/seed/index.js
@@ -16,6 +16,18 @@ const categories = [{
 }, {
   name: 'checkout',
   entries: [{ name: 'Salida al tren'}, { name: 'Salida permanente'}]
+}, {
+  name: 'health',
+  entries: [{ name: 'Revision en hospital'}, { name: 'Hospitalizacion necesaria'}, { name: 'Salida a farmacia'}]
+}, {
+  name: 'social',
+  entries: [{ name: 'Visita a psicologo'}, { name: 'Visita a centro de atencion externo'}]
+}, {
+  name: 'law',
+  entries: [{ name: 'Tramite de documentacion FM4'}, { name: 'Visita a consulado'}, { name: 'Visita a minsiterio publico' }]
+}, {
+  name: 'work',
+  entries: [{ name: 'Trabajo comunitario'}, { name: 'Trabajo en campana de limpieza'}]
 }];
 
 categories.forEach(function(data) {
