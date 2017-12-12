58a29f090ea5659902e995e1a64716169e57f1cd
Cristian Colorado <cristian.colorado@hbr.org>
2017-12-12 13:14:35 -0600

    Fix: Changing view of dashboard

--- services/portal/src/js/scenes/dashboard/screens/dashboard.js
+++ services/portal/src/js/scenes/dashboard/screens/dashboard.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import {VisitorList, AppointmentsList} from 'dashboard';
 
 export default function() {
-  return (
-    <div className="container-flex">
-      <div>
-      <h3 className="p-a-sm">Bienvenido! Esta es la informacion relevante para el dia  de hoy.</h3>
-      <div className="col-sm-6">
-        <VisitorList/>
-      </div>
-      <div className="col-sm-6">
-        <AppointmentsList/>
-      </div>
-      </div>
+  let page = [];
+  page.push(<h3 className="p-a-sm">Bienvenido! Esta es la informacion relevante para el dia  de hoy.</h3>);
+  page.push(<div className="container-flex">
+    <div className="col-sm-6">
+      <VisitorList/>
     </div>
-  );
+    <div className="col-sm-6">
+      <AppointmentsList/>
+    </div>
+  </div>);
+  return page;
 }
